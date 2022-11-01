2e91015495687b3d42284a6dc852f269518d63fe
Cesar Castro <cesarcastro10@outlook.com>
2022-11-01 14:17:29 -0600

    Inicio de la aplicacion con react, jugando con la modularizacion

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navheader from './Navheader.jsx'
+
+function Header() {
+    return (
+        <div>
+
+            <header>
+                <Navheader/>
+            </header>
+        </div>
+
+    );
+}
+
+export default Header
---
+++ src/components/Navheader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navheader() {
+    return (
+        <nav>
+            <a href="http://">Documentacion</a>
+            <a href="http://">Acerca de</a>
+            <a href="http://">Nosotros</a>
+            <a href="http://">Categorias</a>
+            <a href="http://">Informacion</a>
+        </nav>
+    )
+}
+
+export default Navheader
